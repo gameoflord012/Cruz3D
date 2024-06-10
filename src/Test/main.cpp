a88b3681c11c6a00af469443512d8a3ae722e41d
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-06-10 17:59:23 +0700

    setup

---
+++ src/Test/main.cpp
@@ -0,0 +1,28 @@
+#include <ctime>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+int main()
+{
+    try
+    {
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+        glm::vec3 targetPos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+        // Up vector (defines the "up" direction of the camera)
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        // Calculate the lookAt matrix
+        glm::mat4 viewMatrix = glm::lookAt(cameraPos, targetPos, up);
+
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
