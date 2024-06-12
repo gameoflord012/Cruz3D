082db48bef434ba19edc81d92c967c27199375c0
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-06-13 06:49:09 +0700

    assimp

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -1,29 +1,4 @@
-#define CRUZ3D_IMPL
-#include <Cruz3D/Cruz3D.h>
-#undef CRUZ3D_IMPL
-
-#include <glm/gtc/matrix_transform.hpp>
-
-static void sokol_init()
-{
-    sg_setup({
-        .logger = {.func = slog_func},
-        .environment = sglue_environment(),
-    });
-}
-
-static void sokol_frame()
-{
-}
-
-static void sokol_cleanup()
-{
-    sg_shutdown();
-}
-
-static void sokol_event(const sapp_event *)
-{
-}
+#include "assimp_test.h"
 
 sapp_desc sokol_main(int argc, char *argv[])
 {
@@ -33,7 +8,7 @@ sapp_desc sokol_main(int argc, char *argv[])
     return {.init_cb = sokol_init,
             .frame_cb = sokol_frame,
             .cleanup_cb = sokol_cleanup,
-            .event_cb = sokol_event,
+            // .event_cb = sokol_event,
             .width = 640,
             .height = 480,
             .logger = {.func = slog_func}};
