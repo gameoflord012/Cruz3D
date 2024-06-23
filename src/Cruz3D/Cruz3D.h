15e446eaa629f90920ead08a23c1df8d835268b8
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2024-06-23 16:26:00 +0700

    messing with stb and sokol

--- src/Cruz3D/Cruz3D.h
+++ src/Cruz3D/Cruz3D.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <sokol_gfx.h>
+#include <sokol_glue.h>
+#include <sokol_log.h>
+
 #ifdef CRUZ_IMPL
 
 #include "Graphic/Shader.h"
@@ -7,17 +11,8 @@
 #define SOKOL_IMPL
 #define SOKOL_GLCORE
 
-#include "sokol_app.h"
+#include <sokol_app.h>
 #undef SOKOL_APP_IMPL
-
-#include "sokol_gfx.h"
-#undef SOKOL_GFX_IMPL
-
-#include "sokol_glue.h"
-#undef SOKOL_GLUE_IMPL
-
-#include "sokol_log.h"
-#undef SOKOL_LOG_IMPL
 
 #undef SOKOL_IMPL
 #undef SOKOL_GLCORE
---
+++ src/Cruz3D/sokol.cpp
@@ -0,0 +1,8 @@
+#include "sokol_app.h"
+
+#define SOKOL_IMPL
+#define SOKOL_GLCORE
+
+#include "sokol_gfx.h"
+#include "sokol_log.h"
+#include "sokol_glue.h"
